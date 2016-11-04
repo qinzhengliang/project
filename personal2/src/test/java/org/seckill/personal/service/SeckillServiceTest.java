27708d6270881b88fa5eccff99816f32765e5028
qinzhengliang <13451876050@163.com>
2016-11-05 00:19:26 +0800

    sssssss

--- personal2/src/test/java/org/seckill/personal/service/SeckillServiceTest.java
+++ personal2/src/test/java/org/seckill/personal/service/SeckillServiceTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
 public class SeckillServiceTest
 {
+	
+	//ssssssss
     private final Logger logger = LoggerFactory.getLogger(getClass());
     
     @Autowired
